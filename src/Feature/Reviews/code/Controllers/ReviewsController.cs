cc7c0ab76b7773ae3e82e1007d866314a069ea7a
Uladzimir Zaviaruha <Uladzimir_Zaviaruha@epam.com>
2018-03-03 16:14:40 +0300

    Added Reviews feature.

---
+++ src/Feature/Reviews/code/Controllers/ReviewsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MashedPotatoes.Feature.Reviews.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        public ActionResult Reviews()
+        {
+            return this.View();
+        }
+    }
+}
